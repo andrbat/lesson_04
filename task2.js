b8aabd8d56aa8a448b64338ca0cf8b23db720b5e
andrbat <cs050771gap@gmail.com>
2021-12-31 14:50:01 +0200

    add task2

--- task2.js
+++ task2.js
@@ -1,18 +1,51 @@
-const xhr = new XMLHttpRequest();
-xhr.open("GET", "https://jsonplaceholder.typicode.com/users");
-xhr.onreadystatechange = handleResponse;
-xhr.send();
+// const xhr = new XMLHttpRequest();
+// xhr.open("GET", "https://jsonplaceholder.typicode.com/users");
+// xhr.onreadystatechange = handleResponse;
+// xhr.send();
 
-function handleResponse() {
-  if (xhr.readyState === 4 && xhr.status === 200) {
-    addUsersToPage(JSON.parse(xhr.response));
+// function handleResponse() {
+//   if (xhr.readyState === 4 && xhr.status === 200) {
+//     addUsersToPage(JSON.parse(xhr.response));
+//   }
+// }
+
+// function generateListItems(users) {
+//   return users
+//     .map((u) => `<li class="list-group-item">${u.name}</li>`)
+//     .join("");
+// }
+
+// function generateUnorderedList(listItems) {
+//   return `<ul class="list-group">${listItems}</ul>`;
+// }
+
+// function addUsersToPage(users) {
+//   document.getElementById("users").innerHTML = generateUnorderedList(
+//     generateListItems(users)
+//   );
+// }
+
+/*  
+Провести refactoring кода, используя  Promise
+чтобы вызов функций был следующим
+*/
+
+function handleResponse(response) {
+  if (response.status >= 200 && response.status < 300) {
+    return Promise.resolve(response)
+  } else {
+    return Promise.reject(new Error(response.statusText))
   }
 }
 
+function getJSON(url) {
+  return fetch(url)
+    .then(handleResponse)
+    .then(r => r.json());
+}
+
 function generateListItems(users) {
-  return users
-    .map((u) => `<li class="list-group-item">${u.name}</li>`)
-    .join("");
+  return users.map((u) => `<li class="list-group-item">${u.name}</li>`).join("");
 }
 
 function generateUnorderedList(listItems) {
@@ -20,19 +53,12 @@ function generateUnorderedList(listItems) {
 }
 
 function addUsersToPage(users) {
-  document.getElementById("users").innerHTML = generateUnorderedList(
-    generateListItems(users)
-  );
+  document.getElementById("users").innerHTML = users;
 }
 
-/*  
-Провести refactoring кода, используя  Promise
-чтобы вызов функций был следующим
-*/
-
-/*
 getJSON("https://jsonplaceholder.typicode.com/users")
-.then(generateListItems)
-.then(generateUnorderedList)
-.then(addUsersToPage);
-*/
+  .then(generateListItems)
+  .then(generateUnorderedList)
+  .then(addUsersToPage)
+  .catch(error => console.log('Request failed', error));
+
